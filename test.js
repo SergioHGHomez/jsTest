bbbae8c67d97b4830bc3c87e759b822b9852ddd9
SergioHGHomez <rashomez@gmail.com>
2022-05-15 12:53:20 -0500

    agregados operadores logicos

--- test.js
+++ test.js
@@ -9,8 +9,25 @@ myArray.push(25)
 myArray.push("Limit time")
 const mySecondArray = [1,"hola",person,myArray]
 
-
 console.log(coment)
 console.log(person.name)
 console.log(mySecondArray)
+
+// operadores de comparacion
+let comparation = 5 != "5"
+let comparation1 = 5 !== "5"
+
+console.log(comparation, " " , comparation1)
+
+// Operadores logicos 
+// or ||, and && , not !
+
+const opLog1 = true || false
+const opLog2 = false && true
+const opLog3 = !false
+
+console.log(opLog1, opLog2, opLog3 )
+
+
+
 
