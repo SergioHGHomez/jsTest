035b62bc70181d7ace43a17d9c634a0d7c9074e4
SergioHGHomez <rashomez@gmail.com>
2022-05-15 12:05:36 -0500

    added a date to myArray

---
+++ test.js
@@ -0,0 +1,16 @@
+let coment = 'hi'
+const person = {
+	name: 'mike',
+	sureName: 'kaede',
+
+}
+const myArray = []
+myArray.push(25)
+myArray.push("Limit time")
+const mySecondArray = [1,"hola",person,myArray]
+
+
+console.log(coment)
+console.log(person.name)
+console.log(mySecondArray)
+
