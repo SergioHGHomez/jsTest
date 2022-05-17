342062e0c74d9cad37503a1521079899bb3ab052
SergioHGHomez <rashomez@gmail.com>
2022-05-16 20:11:19 -0500

    added arrow funtions to funtions.js

--- funtion.js
+++ funtion.js
@@ -10,7 +10,14 @@ sum(1,2,callback)
 
 
 // fat arrow funtion
-
+// first way of use a arrow function
 const myArrow = (a, b) => a+b
 
 console.log(myArrow(15, 15))
+
+// second way of use a arrow funtion
+const mySecondArrow = (a, b) => {
+    return a + b
+}
+
+console.log( mySecondArrow(15,15))
